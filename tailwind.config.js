b83f88f084f2ace88bf01d829af7adb980031c9e
Arend Böhmer <arend.boehmer@web.de>
2023-12-20 07:47:38 +0100

    Style for last page, optimizations

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,54 +3,6 @@ module.exports = {
   content: ["./templates/*.html"],
   theme: {
     fontSize: {
-      xs: [
-        "0.75rem",
-        {
-          lineHeight: "1rem",
-        },
-      ],
-      sm: [
-        "0.875rem",
-        {
-          lineHeight: "1.5rem",
-        },
-      ],
-      base: [
-        "1rem",
-        {
-          lineHeight: "1.75rem",
-        },
-      ],
-      lg: [
-        "1.125rem",
-        {
-          lineHeight: "2rem",
-        },
-      ],
-      xl: [
-        "1.25rem",
-        {
-          lineHeight: "2rem",
-        },
-      ],
-      "2xl": [
-        "1.5rem",
-        {
-          lineHeight: "2rem",
-        },
-      ],
-      "3xl": [
-        "2rem",
-        {
-          lineHeight: "2.5rem",
-        },
-      ],
-      "4xl": [
-        "2.5rem",
-        {
-          lineHeight: "3.5rem",
-        },
-      ],
       "5xl": [
         "3rem",
         {
